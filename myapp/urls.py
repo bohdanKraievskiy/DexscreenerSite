5c37b3c8362643d3fd2093d9321c8562726f1bcb
bohdanKraievskiy <bogdan.1233211233213@gmail.com>
2024-08-28 09:27:38 +0300

    small upds

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login_view, name='login'),
+    path('waiting', views.login_view, name='login'),
     path('login/entername/', views.enter_name_view, name='enter_name'),
-    path('groups/', views.group_list_view, name='group_list')
+    path('groups/', views.group_list_view, name='group_list'),
+    path('',views.main_view, name='main'),
 ]
